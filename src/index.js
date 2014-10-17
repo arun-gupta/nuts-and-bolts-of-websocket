ff00802789e84a40655b96a53c0ea4889613428f
arun-gupta <arun.gupta@gmail.com>
2014-10-17 12:12:25 -0700

    adding index.js

---
+++ src/index.js
@@ -0,0 +1,30 @@
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var WebSocketServer = require('ws').Server
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	  response.write("Welcome to Node.js on OpenShift!\n\n");
+	  response.end("Thanks for visiting us! \n");
+});
+
+server.listen( port, ipaddress, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wss = new WebSocketServer({
+    server: server,
+    autoAcceptConnections: false
+});
+wss.on('connection', function(ws) {
+  console.log("New connection");
+  ws.on('message', function(message) {
+    ws.send("Received: " + message);
+  });
+  ws.send('Welcome!');
+});
+
+console.log("Listening to " + ipaddress + ":" + port + "...");
